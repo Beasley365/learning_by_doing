8ca48491f71832207e20dfe614d5045f10a4c716
Richard Beasley <richardbeasley27@gmail.com>
2015-10-09 19:45:59 -0600

    Changed string_practice.rb [#105359662]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -5,7 +5,7 @@ practice_string = "this is a string to practice with"
 puts practice_string
 puts practice_string.capitalize
 puts practice_string.upcase
-puts practice_string
-puts practice_string.length
+puts practice_string.capitalize.gsub('string', "'string'")
+puts practice_string.#{}length
 puts practice_string.reverse
 
