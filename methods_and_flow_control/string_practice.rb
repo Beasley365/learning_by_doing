ae25348b0be3f32d3d98b5a7dc5433cf39b4ade8
Richard Beasley <richardbeasley27@gmail.com>
2015-10-11 08:11:22 -0600

    added string_practice.rb [#105359662]

--- methods_and_flow_control/string_practice.rb
+++ methods_and_flow_control/string_practice.rb
@@ -6,6 +6,7 @@ puts practice_string
 puts practice_string.capitalize
 puts practice_string.upcase
 puts practice_string.capitalize.gsub('string', "'string'")
-puts practice_string.#{}length
+puts "The string 'this is a string to practice with' has #{practice_string.length} characters"
 puts practice_string.reverse
+puts "#{practice_string.gsub(practice_string, 'practice ') * 3}".chop.insert(-1, '1')
 
