90f0410ad0bbbd547a9493bc191150b62920a2aa
Richard Beasley <richardbeasley27@gmail.com>
2015-10-26 08:41:33 -0600

    made changes $[10664895]4

---
+++ objects/cash_register.rb
@@ -0,0 +1,26 @@
+# Comment
+class CashRegister
+  attr_reader :total
+
+  def initialize
+    @total = 0.0
+  end
+
+  def purchase(amount)
+    # @total = @total + amount
+    @total += amount
+  end
+
+  def pay(amount)
+    # @total = @total - amount
+    @total -= amount
+    # if total is < or = to zero then print
+    if total <= 0
+      puts "your change is $#{-total}"
+      @total = 0
+    else
+      puts "your new balance is $#{total}"
+    end
+    total
+  end
+end
